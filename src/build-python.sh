a51da3589b89a24a953c75450ab51071c3cb037b
William Stein <wstein@sagemath.com>
2021-07-11 16:50:48 +0000

    add more docs

--- src/build-python.sh
+++ src/build-python.sh
@@ -2,31 +2,61 @@
 set -ev
 cd $BUILD
 
+# Download the Python source code from python.org and extract it:
 curl https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tar.xz -o Python-$PYTHON_VERSION.tar.xz
 tar xvf Python-$PYTHON_VERSION.tar.xz
 cd Python-$PYTHON_VERSION
 
+# Apply all the patches to the code to make it emscripten friendly.
+# These come from pyodide.  They add a WASM cross compilation target
+# to the config scripts (shouldn't that get upstreamed to Python?).
 cat $SRC/python-patches/*.patch | patch -p1
 
+# Also copy the config.site, which answers some questions needed for
+# cross compiling, without which ./configure won't work.
 cp $SRC/config.site .
 
-# We use wasm_32-unknown-emscripten instead of just "wasm32-unknown-emscripten"
-# (which is what pyodide does) so that "make install" below works.
-time CONFIG_SITE=./config.site READELF=true emconfigure ./configure --prefix=$PREFIX \
+# Do the cross-compile ./configure.  Here's an explanation of each
+# of the options we use:
+#     --prefix=$PREFIX: we install the build locally, then copy from there.
+#     --enable-big-digits=30: see pyodide notes.
+#     --enable-optimizations: we want speed (e.g. ,compile with -O3)
+#     --disable-shared: do not need shared library, since WASM doesn't use them
+#     --disable-ipv6: no need, given the WASM sandbox.
+#     --with-ensurepip=no: because otherwise "make install" below fails
+#       trying to do something with pip; obviously, I don't understand this well yet.
+#     --host=wasm32-unknown-emscripten: target we are cross compiling for; matches paches above.
+#     --build=`./config.guess`: the host machine for cross compiling is easily computed via
+#       this config.guess autoconf script.
+time CONFIG_SITE=./config.site READELF=true emconfigure ./configure \
+    --prefix=$PREFIX \
     --enable-big-digits=30 \
     --enable-optimizations \
     --disable-shared \
     --disable-ipv6 \
-    --host=wasm_32-unknown-emscripten \
+    --with-ensurepip=no \
+    --host=wasm32-unknown-emscripten \
     --build=`./config.guess`
 
+# Build Python in parallel...
 time emmake make -j8
 
+# And install it into our local $PREFIX path.
 time emmake make install
 
-# Now gets hackish
 # Rebuild python interpreter with the entire Python library as a filesystem:
-emcc -o python.js Programs/python.o libpython3.9.a -ldl  -lm --lz4   --preload-file $PREFIX/lib/python3.9 -s ALLOW_MEMORY_GROWTH=1 -s ASSERTIONS=0
+# TODO: This is *brittle* and needs to be done better!
+# In particular the "3.9" will break for sure.
+# Options:
+#    --preload-file $PREFIX/lib/python3.9: the Python standard lib, which is needed
+#       for Python to do anything.  This is BIG, unfortunately.
+#    -s ALLOW_MEMORY_GROWTH=1: so Python can use more than a tiny amount of memory
+#    -s ASSERTIONS=0: otherwise *much* use of stubs (related to signals/posix) gets
+#       displayed to stderr.
+emcc -o python.js Programs/python.o libpython3.9.a -ldl  -lm --lz4 \
+     --preload-file $PREFIX/lib/python3.9 \
+     -s ALLOW_MEMORY_GROWTH=1 \
+     -s ASSERTIONS=0
 
 mkdir -p $DIST
 cp python.js python.data python.wasm $DIST
